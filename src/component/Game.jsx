c4ebccc45752b9782f82b05ddd59509aceb4ba25
Ricardo Gimenez <ricardo.gimenez096@gmail.com>
2023-06-27 08:54:36 -0400

    V2

--- src/component/Game.jsx
+++ src/component/Game.jsx
@@ -1,5 +1,5 @@
 import  { useEffect, useState } from 'react'
-import { useNavigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { Square } from "./Square";
 import { players } from "./players";
 import { Turns } from './Turns';
